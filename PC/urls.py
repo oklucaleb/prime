eb07f7c246464c2221c240e94975a5fa1d12075e
oklucaleb <oklucaleb@gmail.com>
2024-02-17 14:00:08 +0000

    forth commit

--- PC/urls.py
+++ PC/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('/about',views.about,name='about'),
+    path('/contat',views.contact,name='contact'),
+    path('/team',views.team,name='team'),
+    path('/testimonial',views.testimonial,name='testimonial'),
+    path('/why',views.why,name='why'),
 ]
